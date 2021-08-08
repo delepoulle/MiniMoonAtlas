32bc2d41509c35f07382dc2793991d8b032b7b05
Samuel Delepoulle <samuel.delepoulle@univ-littoral.fr>
2021-08-08 14:15:21 +0200

    compute phases

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const path = require('path')
 app.use(express.static(__dirname + '/public'))
 app.use('/build/', express.static(path.join(__dirname, 'node_modules/three/build')));
 app.use('/jsm/', express.static(path.join(__dirname, 'node_modules/three/examples/jsm')));
+app.use('/lunarphase/', express.static('node_modules/lunarphase-js/dist'))
 
 
 app.listen(9999, () =>
